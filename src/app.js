c02a25911e60c66f5b8f09f591ff08589d5139ac
venilson1 <venilson2009@hotmail.com>
2022-06-03 17:40:06 -0300

    definition of port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
-
+const port = process.env.PORT || 8080;
 const app = express();
 const httpServer = http.createServer(app);
 const io = new socketio.Server(httpServer);
@@ -20,4 +20,4 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-httpServer.listen(8080);
+httpServer.listen(port);
