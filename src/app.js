9a2e383e8714ce2c53ac3819f932b4487b20544a
venilson1 <venilson2009@hotmail.com>
2022-06-03 17:32:58 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new socketio.Server(httpServer);
+
+app.set("view engine", "ejs");
+
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => console.log(`${socket.id} desconectou`));
+
+  socket.on("message", (data) => {
+    io.emit("responseMessage", data);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+httpServer.listen(8080);
